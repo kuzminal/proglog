ecebe79d1a803c87b588e1fa741146edd04c57b0
kuzminal <kuzmin.al35@gmail.com>
2022-02-07 09:43:38 +0300

    Добавил gRPC, но пока не реазовал заглушки, только схема и сгенеренный код

--- internal/server/log.go
+++ internal/server/log.go
@@ -5,11 +5,13 @@ import (
 	"sync"
 )
 
+// Log Лог состоит из мьютекса для блокировки при записи\чтении и непосредственно записи лога
 type Log struct {
 	mu      sync.Mutex
 	records []Record
 }
 
+//Record состоит из записываемого значения и смещения
 type Record struct {
 	Value  []byte `json:"value"`
 	Offset uint64 `json:"offset"`
@@ -19,6 +21,7 @@ func NewLog() *Log {
 	return &Log{}
 }
 
+//Append Запись в конец лога.
 func (c *Log) Append(record Record) (uint64, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -27,6 +30,7 @@ func (c *Log) Append(record Record) (uint64, error) {
 	return record.Offset, nil
 }
 
+//Read Чтение лога с указанного смещения
 func (c *Log) Read(offset uint64) (Record, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
